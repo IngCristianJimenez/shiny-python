0d7257aff713683c7c5c856d819a502f4e6a5263
Cristian Jimenez <99390740+IngCristianJimenez@users.noreply.github.com>
2024-09-14 08:09:09 -0600

    Add files via upload
    Introduction to Python

---
+++ Exercise number one.py
@@ -0,0 +1,19 @@
+Variables = "containers that hold data values"
+
+Numbers = 'int_a', 'float_b', 'var'
+
+Example_000 = "variable_name = value"
+
+int_a = 3   # int - whole number, such as 1 or -2.
+float_b = 13.2     # float - real number, such as 1.32 or 0.98.
+var = 5
+
+String = 's1', 's2' 
+
+# A char is a single character (For example: 1, 6, %, b, p, ., T, etc.)
+# The str (string) type is a special type that consists of multiple chars.
+
+s1 = 'This is a string'
+s2 = "This is also a string"
+
+print('Hola mapachin')
